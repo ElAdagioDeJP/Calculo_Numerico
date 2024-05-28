09fc5d191c789d225198fa7d85969fbc0986b50a
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-05-28 16:52:22 -0400

    Program to list of buying

--- Calculo_Numerico/Ejercicios/R_Proyect.r
+++ Calculo_Numerico/Ejercicios/R_Proyect.r
@@ -2,3 +2,26 @@ x <- 18
 y <- 2
 x + y
 print (x - y)
+print('*********************************************')
+print('**********Bienvenidos al enlistador**********')
+print('*********************************************')
+
+print()
+print("Bienvenidos al programa de listas de compras")
+
+lista_de_compras <- c()
+
+productos <- readline(prompt="Ingrese un producto: ")
+
+while (productos != "exit") {
+    lista_de_compras <- c(lista_de_compras, productos)
+    print()
+    productos <- readline(prompt="Ingrese un producto (exit): ")
+}
+
+print("Tu lista de compras es:")
+print(lista_de_compras)
+
+
+
+
