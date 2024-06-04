ed0eaa5c9da7af5144f9bc0eec98d5139675c3a7
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-06-03 23:23:29 -0400

    Testing

--- Calculo_Numerico/Ejercicios/Matplotlib_Grafica3D.py
+++ Calculo_Numerico/Ejercicios/Matplotlib_Grafica3D.py
@@ -1,3 +1,11 @@
-import matplotlib as plt
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-xd
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+[x,y,z] = X0 = [0,0,0]
+ax.scatter(x, y, z, c='r', marker='o')
+
+plt.show()
