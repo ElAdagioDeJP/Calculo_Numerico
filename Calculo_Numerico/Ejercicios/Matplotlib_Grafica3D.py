33b761b75d1d75057b08062b427315762e02ed3a
ElAdagioDeJP <129967733+ElAdagioDeJP@users.noreply.github.com>
2024-06-05 12:31:30 -0400

    Finished in 0.000s

--- Calculo_Numerico/Ejercicios/Matplotlib_Grafica3D.py
+++ Calculo_Numerico/Ejercicios/Matplotlib_Grafica3D.py
@@ -1,11 +1,25 @@
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-import numpy as np
 
+# Generar una matriz aleatoria
+A = np.random.rand(5,5)
+
+# Crear una malla de coordenadas
+x = np.arange(A.shape[1])
+y = np.arange(A.shape[0])
+x, y = np.meshgrid(x, y)
+
+# Crear una figura 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-[x,y,z] = X0 = [0,0,0]
-ax.scatter(x, y, z, c='r', marker='o')
+ax.set_xlabel('eje x')
+ax.set_ylabel('eje y')
+ax.set_zlabel('eje z')
+ax.legend()
+# Graficar la superficie
+ax.plot_surface(x, y, A, cmap='hot')
 
+# Mostrar la figura
 plt.show()
